d0ea2cb5fee4010cc1a39a1666ace64908e4d23d
Vladimir Zarypov <krre31@gmail.com>
2023-10-05 19:11:47 +0300

    Add nav menu

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,8 +2,19 @@
 </script>
 
 <header>
-	<nav />
+	<nav>
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/">Sign in</a></li>
+			<li><a href="/">Sign up</a></li>
+		</ul>
+	</nav>
 </header>
 
 <style>
+	ul {
+		display: flex;
+		list-style: none;
+		column-gap: 1em;
+	}
 </style>
