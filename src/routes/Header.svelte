412c75b8ff428257ed17793394ed0345f73e1f52
Vladimir Zarypov <krre31@gmail.com>
2024-05-18 06:24:34 +0300

    Add spacer between 'Home' and rest menu elements

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -5,6 +5,7 @@
 	<nav>
 		<ul>
 			<li><a href="/">Home</a></li>
+			<div class="spacer"></div>
 			<li><a href="/signin">Sign in</a></li>
 			<li><a href="/signup">Sign up</a></li>
 		</ul>
@@ -16,5 +17,10 @@
 		display: flex;
 		list-style: none;
 		column-gap: 1em;
+		padding-inline-end: 40px;
+	}
+
+	.spacer {
+		flex-grow: 1;
 	}
 </style>
